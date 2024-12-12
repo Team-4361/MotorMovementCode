d6d2bc4c090500556558bc63fc3de6e1b65e1379
PhoebeL27 <plalo27@students.roxbury.org>
2024-12-12 15:32:01 -0500

    actual code

--- MotorMovementCode/src/main/java/frc/robot/Robot.java
+++ MotorMovementCode/src/main/java/frc/robot/Robot.java
@@ -1,106 +1,99 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
 
+// Import necessary WPILib and REV Robotics libraries
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+import com.revrobotics.SparkPIDController;
+import com.revrobotics.RelativeEncoder;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.XboxController;
 
-/**
- * The VM is configured to automatically run this class, and to call the functions corresponding to
- * each mode, as described in the TimedRobot documentation. If you change the name of this class or
- * the package after creating this project, you must also update the build.gradle file in the
- * project.
- */
 public class Robot extends TimedRobot {
-  private static final String kDefaultAuto = "Default";
-  private static final String kCustomAuto = "My Auto";
-  private String m_autoSelected;
-  private final SendableChooser<String> m_chooser = new SendableChooser<>();
 
-  /**
-   * This function is run when the robot is first started up and should be used for any
-   * initialization code.
-   */
-  @Override
-  public void robotInit() {
-    m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
-    m_chooser.addOption("My Auto", kCustomAuto);
-    SmartDashboard.putData("Auto choices", m_chooser);
-  }
+    // Declare motor, controller, and encoder
+    private CANSparkMax sparkMax;
 
-  /**
-   * This function is called every 20 ms, no matter the mode. Use this for items like diagnostics
-   * that you want ran during disabled, autonomous, teleoperated and test.
-   *
-   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
-   * SmartDashboard integrated updating.
-   */
-  @Override
-  public void robotPeriodic() {}
+    private RelativeEncoder encoder;
 
-  /**
-   * This autonomous (along with the chooser code above) shows how to select between different
-   * autonomous modes using the dashboard. The sendable chooser code works with the Java
-   * SmartDashboard. If you prefer the LabVIEW Dashboard, remove all of the chooser code and
-   * uncomment the getString line to get the auto name from the text box below the Gyro
-   *
-   * <p>You can add additional auto modes by adding additional comparisons to the switch structure
-   * below with additional strings. If using the SendableChooser make sure to add them to the
-   * chooser code above as well.
-   */
-  @Override
-  public void autonomousInit() {
-    m_autoSelected = m_chooser.getSelected();
-    // m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
-    System.out.println("Auto selected: " + m_autoSelected);
-  }
+    // Set CAN ID for the SPARK MAX
+    private static final int SPARK_MAX_CAN_ID = 28;
 
-  /** This function is called periodically during autonomous. */
-  @Override
-  public void autonomousPeriodic() {
-    switch (m_autoSelected) {
-      case kCustomAuto:
-        // Put custom auto code here
-        break;
-      case kDefaultAuto:
-      default:
-        // Put default auto code here
-        break;
+    // PID coefficients
+    private static final double kP = 0.1;
+    private static final double kI = 0.0;
+    private static final double kD = 0.0;
+    private static final double kIz = 0.0;
+    private static final double kFF = 0.0;
+    private static final double kMaxOutput = 1.0;
+    private static final double kMinOutput = -1.0;
+
+    // Target position for the motor (in encoder units)
+    private double targetPosition = 0.0;
+
+    // Xbox controller for input
+    private XboxController xboxController;
+
+    @Override
+    public void robotInit() {
+        // Initialize motor
+        sparkMax = new CANSparkMax(SPARK_MAX_CAN_ID, MotorType.kBrushless);
+
+        // Restore factory defaults to ensure consistent behavior
+        sparkMax.restoreFactoryDefaults();
+
+        // Get the PID controller and encoder
+        //pidController = sparkMax.getPIDController();
+        encoder = sparkMax.getEncoder();
+
+        // Configure PID coefficients
+        //pidController.setP(kP);
+        //pidController.setI(kI);
+        //pidController.setD(kD);
+        //pidController.setIZone(kIz);
+        //pidController.setFF(kFF);
+        //pidController.setOutputRange(kMinOutput, kMaxOutput);
+
+        // Initialize Xbox controller (USB port 0)
+        xboxController = new XboxController(0);
     }
-  }
 
-  /** This function is called once when teleop is enabled. */
-  @Override
-  public void teleopInit() {}
+    @Override
+    public void teleopPeriodic() {
+        // Check if A button is pressed to increment position
+        if (xboxController.getYButtonPressed()) {
+            targetPosition += 50.0; // Increment target position
+            while (encoder.getPosition()<targetPosition)
+              sparkMax.set(0.1);
+            sparkMax.set(0);
+            
+        }
 
-  /** This function is called periodically during operator control. */
-  @Override
-  public void teleopPeriodic() {}
+        // Check if B button is pressed to decrement position
+        if (xboxController.getAButtonPressed()) {
+          if (targetPosition >= 50)
+          {
+            targetPosition -= 50.0; // Decrement target position
+            while (encoder.getPosition()>targetPosition && encoder.getPosition()>1)
+              sparkMax.set(-0.1);
+            sparkMax.set(0);
+          }
+        }
 
-  /** This function is called once when the robot is disabled. */
-  @Override
-  public void disabledInit() {}
+        // Send target position to the PID controller
+       // pidController.setReference(targetPosition, CANSparkMax.ControlType.kPosition);
 
-  /** This function is called periodically when disabled. */
-  @Override
-  public void disabledPeriodic() {}
+        // Print current and target positions for debugging
 
-  /** This function is called once when test mode is enabled. */
-  @Override
-  public void testInit() {}
+   
 
-  /** This function is called periodically during test mode. */
-  @Override
-  public void testPeriodic() {}
+        System.out.println("Current Position: " + encoder.getPosition());
+        System.out.println("Target Position: " + targetPosition);
+    }
 
-  /** This function is called once when the robot is first started up. */
-  @Override
-  public void simulationInit() {}
-
-  /** This function is called periodically whilst in simulation. */
-  @Override
-  public void simulationPeriodic() {}
+    @Override
+    public void disabledInit() {
+        // Stop the motor when disabled
+        sparkMax.stopMotor();
+    }
 }
+
