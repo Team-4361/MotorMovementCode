0f79247fe63a23a4388cd67a8154a131a89da9c4
SahPatil <spatil28@students.roxbury.org>
2025-03-06 14:22:24 -0500

    coralpid update and videostream stuff

--- CoralPID/src/main/java/frc/robot/commands/coral/BucketMoveToPosition.java
+++ CoralPID/src/main/java/frc/robot/commands/coral/BucketMoveToPosition.java
@@ -3,25 +3,30 @@ package frc.robot.commands.coral;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.BucketSubsystem;
 
-public class bucketmove extends Command {
+public class BucketMoveToPosition extends Command {
     private final BucketSubsystem bucket;
+    private final double targetDegrees;
 
-    public bucketmove(BucketSubsystem bucket) {
+    public BucketMoveToPosition(BucketSubsystem bucket, double degrees) {
         this.bucket = bucket;
-
+        this.targetDegrees = degrees;
         addRequirements(bucket);
     }
 
     @Override
     public void initialize() {
-        bucket.resetEncoder();
+        bucket.setPosition(targetDegrees);
     }
 
     @Override
     public void execute() {
-        bucket.resetEncoder();
+        bucket.setPosition(targetDegrees);
     }
 
+    @Override
+    public boolean isFinished() {
+        return bucket.atSetpoint(); // Check if it's at the target position
+    }
 
     @Override
     public void end(boolean interrupted) {
