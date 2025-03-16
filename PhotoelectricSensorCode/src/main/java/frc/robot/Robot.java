ecf5b6520b3c9b5c9acc052f9bcf9834e40dae35
PhoebeL27 <plalo27@students.roxbury.org>
2025-03-16 15:33:41 -0400

    commented all code; ready for production

--- PhotoelectricSensorCode/src/main/java/frc/robot/Robot.java
+++ PhotoelectricSensorCode/src/main/java/frc/robot/Robot.java
@@ -1,14 +1,13 @@
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
+//IDK how to change this file?
 
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import frc.robot.subsystems.PhotoelectricSensorSubsystem;
-
 
 /**
  * The methods in this class are called automatically corresponding to each mode, as described in
@@ -56,12 +55,12 @@ public class Robot extends TimedRobot {
   /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
   @Override
   public void autonomousInit() {
-    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+   /*  m_autonomousCommand = m_robotContainer.getAutonomousCommand();
 
     // schedule the autonomous command (example)
     if (m_autonomousCommand != null) {
       m_autonomousCommand.schedule();
-    }
+    }*/
   }
 
   /** This function is called periodically during autonomous. */
