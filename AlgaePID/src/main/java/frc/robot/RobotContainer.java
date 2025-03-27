6d86ae0a2f7c175c2939d9abcc791c04bf09afa4
SahPatil <spatil28@students.roxbury.org>
2025-03-27 19:31:12 -0400

    Updated BucketPID with PID now and updated subsystem from actual code and stuff

--- AlgaePID/src/main/java/frc/robot/RobotContainer.java
+++ AlgaePID/src/main/java/frc/robot/RobotContainer.java
@@ -5,9 +5,8 @@
 package frc.robot;
 
 import frc.robot.Constants.OperatorConstants;
-import frc.robot.commands.coral.BucketMoveToPosition;
-import frc.robot.commands.coral.bucketmove;
 import frc.robot.subsystems.BucketSubsystem;
+import frc.robot.commands.coral.BucketIntakeCommand;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
@@ -43,10 +42,7 @@ public class RobotContainer {
    */
   private void configureBindings() {
     // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
-    driverXbox.povLeft().whileTrue(new BucketMoveToPosition(bucket, -5)); // Move bucket to -45 degrees
-    driverXbox.povRight().whileTrue(new BucketMoveToPosition(bucket, 5)); // Move bucket to 45 degrees
-    driverXbox.povUp().whileTrue(new BucketMoveToPosition(bucket, 0)); // Move bucket to 45 degrees
-    driverXbox.povDown().whileTrue(new bucketmove(bucket)); // Move bucket to 45 degrees
+    driverXbox.povLeft().onTrue(new BucketIntakeCommand(bucket));
 
   }
 
